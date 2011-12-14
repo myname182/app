35fad68cadc91e179f8734754eada8b55ea50b46
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-12-14 12:11:57 -0700

    Updated

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -3,8 +3,6 @@ using System.Linq;
 
 namespace app.web.core
 {
-    using System;
-
   public class CommandRegistry : IFindCommands
   {
     IEnumerable<IProcessASingleRequest> commands;
@@ -12,20 +10,13 @@ namespace app.web.core
 
     public CommandRegistry(IEnumerable<IProcessASingleRequest> commands, IProcessASingleRequest specialCase)
     {
-        this.commands = commands;
-        special_case = specialCase;
+      this.commands = commands;
+      special_case = specialCase;
     }
 
     public IProcessASingleRequest get_the_command_that_can_process(IProvideDetailsToCommands request)
     {
-        try
-        {
-            return commands.First(x => x.can_process(request));
-        }
-        catch(InvalidOperationException)
-        {
-            return special_case;
-        }
+      return commands.FirstOrDefault(x => x.can_process(request)) ?? special_case;
     }
   }
 }
