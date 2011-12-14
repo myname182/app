d2c43a30b8acfd02b6d88808c6bfb59b07b6f4b4
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-12-14 11:39:33 -0700

    Updated

--- source/app.specs/CommandRegistrySpecs.cs
+++ source/app.specs/CommandRegistrySpecs.cs
@@ -46,7 +46,31 @@ namespace app.specs
         static IProvideDetailsToCommands request;
         static IList<IProcessASingleRequest> all_commands;
       }
-        
+         
+      public class and_it_does_not_have_the_command:when_finding_a_command_for_a_request
+      {
+        Establish c = () =>
+        {
+          the_special_case = depends.on<IProcessASingleRequest>();
+          all_commands = Enumerable.Range(1,100).Select(x => fake.an<IProcessASingleRequest>()).ToList();
+          request = fake.an<IProvideDetailsToCommands>();
+
+          depends.on<IEnumerable<IProcessASingleRequest>>(all_commands);
+        };
+
+        Because b = () =>
+          result = sut.get_the_command_that_can_process(request);
+
+
+        It should_return_the_special_case = () =>
+          result.ShouldEqual(the_special_case);
+
+
+        static IProcessASingleRequest result;
+        static IProvideDetailsToCommands request;
+        static IList<IProcessASingleRequest> all_commands;
+        static IProcessASingleRequest the_special_case;
+      }
     }
   }
 }
