f2d4f7925d7cd537db0595149c67e70ccf631992
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-12-13 20:34:12 -0700

    Updated

--- source/app.specs/CalculatorSpecs.cs
+++ source/app.specs/CalculatorSpecs.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using Machine.Specifications;
 using developwithpassion.specifications.rhinomocks;
 using developwithpassion.specifications.extensions;
@@ -21,16 +22,26 @@ namespace app.specs
         spec.exception_thrown.ShouldBeAn<ArgumentException>();
 
     } 
+
     public class when_adding_two_positive_numbers :concern
     {
+      Establish c = () =>
+      {
+        connection = depends.on<IDbConnection>();
+      };
+
       Because b = () =>
         result = sut.add(2, 3);
 
+      It should_open_a_connection_to_the_database = () =>
+        connection.received(x => x.Open());
+        
 
       It should_return_the_sum = () =>
         result.ShouldEqual(5);
 
       static int result;
+      static IDbConnection connection;
     } 
   }
 }
--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.Data;
+using System.Linq;
 
 namespace app
 {
   public class Calculator
   {
+    public Calculator(IDbConnection connection)
+    {
+    }
+
     public int add(int first, int second)
     {
-        if(first < 0 || second < 0)
-            throw new ArgumentException("no!");
-        return first + second;
+      ensure_all_are_positive(first, second);
+
+      return first + second;
+    }
+
+    static void ensure_all_are_positive(params int[] numbers)
+    {
+      if(numbers.Any(x => x<0)) throw new ArgumentException("no!");
     }
   }
 }
