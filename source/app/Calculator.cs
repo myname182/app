8999651d29b5bf2907bb8481319730d47b47f00f
myname182 <topeter@gmail.com>
2011-12-13 19:25:11 -0800

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,13 @@
-﻿namespace app
+﻿using System;
+
+namespace app
 {
   public class Calculator
   {
     public int add(int first, int second)
     {
+        if(first < 0 || second < 0)
+            throw new ArgumentException("no!");
         return first + second;
     }
   }
