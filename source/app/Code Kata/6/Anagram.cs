5e0ec6953b4f76f59cb32a2eca6553b185d3bc51
myname182 <topeter@gmail.com>
2011-12-14 09:01:57 -0800

    Pushing new changes

---
+++ source/app/Code Kata/6/Anagram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace app.Code_Kata._6
+{
+    public class Anagram
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"c:\Words.txt");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
