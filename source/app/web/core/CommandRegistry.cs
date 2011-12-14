b38221d22d1104089883a0e5bb6c964ba4c6cfd2
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-12-14 11:50:03 -0700

    Updated

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -1,20 +1,20 @@
-﻿namespace app.web.core
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace app.web.core
 {
-    using System.Collections.Generic;
-    using System.Linq;
-
-    public class CommandRegistry:IFindCommands
+  public class CommandRegistry : IFindCommands
   {
-      IEnumerable<IProcessASingleRequest> request_processors; 
+    IEnumerable<IProcessASingleRequest> commands;
 
-      public CommandRegistry(IEnumerable<IProcessASingleRequest> requestProcessors)
-      {
-          request_processors = requestProcessors;
-      }
+    public CommandRegistry(IEnumerable<IProcessASingleRequest> commands)
+    {
+      this.commands = commands;
+    }
 
-      public IProcessASingleRequest get_the_command_that_can_process(IProvideDetailsToCommands request)
-      {
-          return request_processors.First(x => x.can_process(request));
-      }
+    public IProcessASingleRequest get_the_command_that_can_process(IProvideDetailsToCommands request)
+    {
+      return commands.First(x => x.can_process(request));
+    }
   }
 }
