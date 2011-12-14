efde35e81e61913809b95b69cd56083536625777
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-12-14 14:09:26 -0700

    Updated

--- source/app.specs/ViewTheDepartmentsInADepartmentSpecs.cs
+++ source/app.specs/ViewTheDepartmentsInADepartmentSpecs.cs
@@ -1,47 +1,44 @@
-﻿ using System.Collections.Generic;
- using Machine.Specifications;
- using app.web.application.catalogbrowsing;
- using app.web.core;
- using developwithpassion.specifications.rhinomocks;
- using developwithpassion.specifications.extensions;
+﻿using System.Collections.Generic;
+using Machine.Specifications;
+using app.web.application.catalogbrowsing;
+using app.web.core;
+using developwithpassion.specifications.extensions;
+using developwithpassion.specifications.rhinomocks;
 
 namespace app.specs
-{  
-  [Subject(typeof(ViewTheDepartmentsInADepartment))]  
+{
+  [Subject(typeof(ViewTheDepartmentsInADepartment))]
   public class ViewTheDepartmentsInADepartmentSpecs
   {
     public abstract class concern : Observes<ISupportAStory,
                                       ViewTheDepartmentsInADepartment>
     {
-        
     }
 
-   
     public class when_run : concern
     {
-        Establish c = () =>
-        {
-            department_repository = depends.on<IFindDepartments>();
-            display_engine = depends.on<IDisplayReports>();
-            request = fake.an<IProvideDetailsToCommands>();
-            the_main_department = new DepartmentItem();
-            the_sub_departments = the_main_department.departments;
+      Establish c = () =>
+      {
+        department_repository = depends.on<IFindDepartments>();
+        display_engine = depends.on<IDisplayReports>();
+        request = fake.an<IProvideDetailsToCommands>();
+        the_main_department = new DepartmentItem();
+        the_sub_departments = new List<DepartmentItem> {new DepartmentItem()};
 
-            department_repository.setup(x => x.get_the_main_departments()).Return(the_sub_departments);
-        };
+        department_repository.setup(x => x.get_the_departments_in(the_main_department)).Return(the_sub_departments);
+      };
 
+      Because b = () =>
+        sut.process(request);
 
-        Because b = () =>
-       sut.process(request);
+      It should_display_the_departments_in_a_department = () =>
+        display_engine.received(x => x.display(the_main_department.departments));
 
-        It should_display_the_departments_in_a_department = () =>
-            display_engine.received(x => x.display(the_main_department.departments));
-
-        static IFindDepartments department_repository;
-        static IProvideDetailsToCommands request;
-        static IDisplayReports  display_engine;
-        static DepartmentItem the_main_department;
-        static IEnumerable<DepartmentItem> the_sub_departments;
+      static IFindDepartments department_repository;
+      static IProvideDetailsToCommands request;
+      static IDisplayReports display_engine;
+      static DepartmentItem the_main_department;
+      static IEnumerable<DepartmentItem> the_sub_departments;
     }
   }
 }
--- source/app/web/application/catalogbrowsing/IFindDepartments.cs
+++ source/app/web/application/catalogbrowsing/IFindDepartments.cs
@@ -5,5 +5,6 @@ namespace app.web.application.catalogbrowsing
   public interface IFindDepartments
   {
     IEnumerable<DepartmentItem> get_the_main_departments();
+    IEnumerable<DepartmentItem> get_the_departments_in(DepartmentItem parent_department);
   }
 }
