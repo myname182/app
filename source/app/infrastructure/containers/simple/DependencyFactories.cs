df19195d738089e337f05660415bd41cf64a5e88
Brandon Slobig <bdslobig@gmail.com>
2011-12-15 15:49:37 -0600

    Pushing new changes

--- source/app/infrastructure/containers/simple/DependencyFactories.cs
+++ source/app/infrastructure/containers/simple/DependencyFactories.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace app.infrastructure.containers.simple
 {
   public class DependencyFactories : IFindFactoriesThatCanCreateDependencies
   {
-    public ICreateASingleDependency get_factory_that_can_create(Type dependency)
-    {
-      throw new NotImplementedException();
-    }
+	  private IEnumerable<ICreateASingleDependency> asd;
+
+	  public DependencyFactories(IEnumerable<ICreateASingleDependency> asd)
+	  {
+		  this.asd = asd;
+	  }
+
+
+
+	  public ICreateASingleDependency get_factory_that_can_create(Type dependency)
+	  {
+	  	return asd.FirstOrDefault(x => x.can_create(dependency));
+	  }
   }
 }
