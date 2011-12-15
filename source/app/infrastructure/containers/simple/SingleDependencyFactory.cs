0ebce883305a8424a3725630e7d64db686981ff2
arturomoreno <artu_dee2@hotmail.com>
2011-12-15 14:58:36 -0800

    Pushing new changes

--- source/app/infrastructure/containers/simple/SingleDependencyFactory.cs
+++ source/app/infrastructure/containers/simple/SingleDependencyFactory.cs
@@ -5,16 +5,17 @@ namespace app.infrastructure.containers.simple
 	public class SingleDependencyFactory : ICreateASingleDependency
 	{
 		private IMatchAType type_criteria;
+	    private ICreateADependency real_factory;
 
-		public SingleDependencyFactory(IMatchAType type_criteria)
+		public SingleDependencyFactory(IMatchAType type_criteria, ICreateADependency real_factory)
 		{
 			this.type_criteria = type_criteria;
-
+		    this.real_factory = real_factory;
 		}
 
 		public object create()
 		{
-			throw new NotImplementedException();
+		    return real_factory.create();
 		}
 
 		public bool can_create(Type type)
