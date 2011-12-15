d3786d5f812697065f3a42804cdb0ed27a0cecf3
Brandon Slobig <bdslobig@gmail.com>
2011-12-15 17:20:34 -0600

    Pushing new changes

--- source/app/infrastructure/containers/simple/BasicDependencyFactory.cs
+++ source/app/infrastructure/containers/simple/BasicDependencyFactory.cs
@@ -1,10 +1,19 @@
-﻿namespace app.infrastructure.containers.simple
+﻿using System;
+
+namespace app.infrastructure.containers.simple
 {
   public class BasicDependencyFactory : ICreateADependency
   {
-    public object create()
+  	private Func<object> the_connection;
+
+	  public BasicDependencyFactory(Func<object> the_connection)
+	  {
+	  	this.the_connection = the_connection;
+	  }
+
+  	public object create()
     {
-      throw new System.NotImplementedException();
+      return the_connection.Invoke();
     }
   }
 }
