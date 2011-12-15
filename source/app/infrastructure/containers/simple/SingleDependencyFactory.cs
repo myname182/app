e668b48872d03a3f5304d24b5d241e8ec20b7eeb
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-12-15 16:00:25 -0700

    Updated

--- source/app/infrastructure/containers/simple/SingleDependencyFactory.cs
+++ source/app/infrastructure/containers/simple/SingleDependencyFactory.cs
@@ -2,25 +2,25 @@
 
 namespace app.infrastructure.containers.simple
 {
-	public class SingleDependencyFactory : ICreateASingleDependency
-	{
-		private IMatchAType type_criteria;
-	    private ICreateADependency real_factory;
+  public class SingleDependencyFactory : ICreateASingleDependency
+  {
+    IMatchAType type_criteria;
+    ICreateADependency real_factory;
 
-		public SingleDependencyFactory(IMatchAType type_criteria, ICreateADependency real_factory)
-		{
-			this.type_criteria = type_criteria;
-		    this.real_factory = real_factory;
-		}
+    public SingleDependencyFactory(IMatchAType type_criteria, ICreateADependency real_factory)
+    {
+      this.type_criteria = type_criteria;
+      this.real_factory = real_factory;
+    }
 
-		public object create()
-		{
-		    return real_factory.create();
-		}
+    public object create()
+    {
+      return real_factory.create();
+    }
 
-		public bool can_create(Type type)
-		{
-			return type_criteria.matches(type);
-		}
-	}
+    public bool can_create(Type type)
+    {
+      return type_criteria.matches(type);
+    }
+  }
 }
