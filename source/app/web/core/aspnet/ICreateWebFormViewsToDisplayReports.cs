df63c75c01cff48d2d7dac89b2e2f12a1bb70728
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-12-14 19:20:34 -0700

    Updated

---
+++ source/app/web/core/aspnet/ICreateWebFormViewsToDisplayReports.cs
@@ -0,0 +1,7 @@
+﻿namespace app.web.core.aspnet
+{
+  public interface ICreateWebFormViewsToDisplayReports
+  {
+    void create_view_that_can_display<ReportModel>(ReportModel the_report);
+  }
+}
---
+++ source/app/web/core/aspnet/WebFormDisplayEngine.cs
@@ -0,0 +1,10 @@
+﻿namespace app.web.core.aspnet
+{
+  public class WebFormDisplayEngine : IDisplayReports
+  {
+    public void display<Report>(Report report)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
