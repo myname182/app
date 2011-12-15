6b2ebf4851ae90bbb5a9b81ae45a143e9cab6415
myname182 <topeter@gmail.com>
2011-12-15 08:42:00 -0800

    Pushing new changes

--- source/app/Code Kata/6/Anagram.cs
+++ source/app/Code Kata/6/Anagram.cs
@@ -11,12 +11,23 @@ namespace app.Code_Kata._6
         static void Main(string[] args)
         {
             var lines = File.ReadAllLines(@"c:\Words.txt");
+            var dictionary = File.ReadAllLines(@"c:\dictionary.txt");
             foreach (var line in lines)
             {
-                Console.WriteLine(line);
+                var cleanLine = line.Replace(" ", "");
+                foreach (var dLine in dictionary)
+                {
+                    while(fin)
+                }
             }
             Console.ReadLine();
         }
-
+        private bool findMatch(char character, int currentIndex, string dictionaryWord)
+        {
+            if (dictionaryWord[currentIndex] == character)
+                return true;
+            return false;
+        }
+        string[][] allPossibleMatches (string[] word)
     }
 }
