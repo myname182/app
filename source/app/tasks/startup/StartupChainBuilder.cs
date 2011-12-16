d341f00c90ae7eec72e0f51c266bd45bdc6367c9
Brandon Slobig <bdslobig@gmail.com>
2011-12-16 14:23:53 -0600

    Pushing new changes

--- source/app/tasks/startup/StartupChainBuilder.cs
+++ source/app/tasks/startup/StartupChainBuilder.cs
@@ -1,7 +1,16 @@
-﻿namespace app.tasks.startup
+﻿using System;
+using System.Collections.Generic;
+
+namespace app.tasks.startup
 {
   public class StartupChainBuilder : ICreateStartupChains
   {
-     
+  	private IList<Type> all_steps;
+
+     public StartupChainBuilder(IList<Type> all_steps, Type first_step)
+     {
+     	this.all_steps = all_steps;
+		 all_steps.Add(first_step);
+     }
   }
 }
