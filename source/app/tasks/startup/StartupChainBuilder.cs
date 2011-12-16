7e0b26322de170243f61f0e822f96a4afec4143d
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-12-16 14:30:46 -0700

    Updated

--- source/app/tasks/startup/StartupChainBuilder.cs
+++ source/app/tasks/startup/StartupChainBuilder.cs
@@ -1,23 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using app.infrastructure.containers;
 using app.tasks.startup.steps;
 
 namespace app.tasks.startup
 {
-    using app.infrastructure.containers;
-
-    public class StartupChainBuilder : ICreateStartupChains
+  public class StartupChainBuilder : ICreateStartupChains
   {
     public IList<Type> all_steps;
 
-      private ICreateAStartupStep startup_step;
+    ICreateAStartupStep step_factory;
 
-     public StartupChainBuilder(IList<Type> all_steps, Type first_step, ICreateAStartupStep startupStep)
-      {
-        this.all_steps = all_steps;
-          startup_step = startupStep;
-          add(first_step);
-      }
+    public StartupChainBuilder(IList<Type> all_steps, Type first_step, ICreateAStartupStep step_factory)
+    {
+      this.all_steps = all_steps;
+      step_factory = step_factory;
+      add(first_step);
+    }
 
     void add(Type step)
     {
@@ -30,19 +29,16 @@ namespace app.tasks.startup
       return all_steps.Contains(step);
     }
 
-    public void finish_by<AStartupStep>() where AStartupStep : IRunAStartupStep    
+    public void finish_by<AStartupStep>() where AStartupStep : IRunAStartupStep
     {
-        startup_step.create_step_from(typeof(AStartupStep)).run();
-
-        foreach (var step in all_steps)
-        {
-            startup_step.create_step_from(step).run();
-        }
+      add(typeof(AStartupStep));
+      foreach (var step in all_steps) step_factory.create_step_from(step).run();
     }
 
     public ICreateStartupChains followed_by<AStartupStep>() where AStartupStep : IRunAStartupStep
     {
-      return new StartupChainBuilder(new List<Type>(all_steps), typeof(AStartupStep), Container.fetch.an<ICreateAStartupStep>());
+      return new StartupChainBuilder(new List<Type>(all_steps), typeof(AStartupStep),
+                                     Container.fetch.an<ICreateAStartupStep>());
     }
   }
 }
