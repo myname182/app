d055ee2463fe8ad896f19bf2b891fa1c2985a701
Brandon Slobig <bdslobig@gmail.com>
2011-12-15 20:06:53 -0600

    Pushing new changes

--- source/app/infrastructure/containers/Container.cs
+++ source/app/infrastructure/containers/Container.cs
@@ -4,10 +4,7 @@ namespace app.infrastructure.containers
 {
   public class Container
   {
-    public static ContainerFacadeResolver facade_resolver = () =>
-    {
-      throw new NotImplementedException("This needs to be configured by a startup process");
-    };
+    public static ContainerFacadeResolver facade_resolver = () => fetch;
 
     public static IFetchDependencies fetch
     {
--- source/app/tasks/startup/Startup.cs
+++ source/app/tasks/startup/Startup.cs
@@ -6,34 +6,58 @@ using app.web.core;
 
 namespace app.tasks.startup
 {
-  public class Startup
-  {
-    static IList<ICreateASingleDependency> all_dependencies;
+	public class Startup
+	{
+		static IList<ICreateASingleDependency> all_dependencies;
 
-    public static void run()
-    {
-      all_dependencies = new List<ICreateASingleDependency>();
+		private delegate bool blah(int asd, int sd);
+		private Func<int> asd ;
 
-      IFetchDependencies container =
-        new SimpleContainer(
-          new DependencyFactories(all_dependencies,
-                                  type => new NotImplementedException(
-                                    string.Format("There is no factory registered that can create {0}", type.Name))),
-          (inner, type) =>
-            new NotImplementedException(string.Format("There was an error attempting to create a {0}", type.Name), inner));
+		public static void run()
+		{
+			//new
+			//Container.facade_resolver = new ContainerFacadeResolver();
+			// end new
+			all_dependencies = new List<ICreateASingleDependency>();
 
-      ContainerFacadeResolver resolver = () => container;
-      Container.facade_resolver = resolver;
+			IFetchDependencies container =
+			  new SimpleContainer(
+				new DependencyFactories(all_dependencies,
+										type => new NotImplementedException(
+										  "There is no factory registered that can create {0}".format(type.Name))),
+				(inner, type) => //itemcreationexceptionfactory delegate
+				  new NotImplementedException("There was an error attempting to create a {0}".format(type.Name), inner));
 
-      populate_all_dependency_factories();
+			ContainerFacadeResolver resolver = () => container;
+			Container.facade_resolver = resolver;
+
+			populate_all_dependency_factories();
 
 
-    }
+			//ref
+			//all_dependencies = new List<ICreateASingleDependency>();
 
-    static void populate_all_dependency_factories()
-    {
-      all_dependencies = new List<ICreateASingleDependency>();
-      all_dependencies.Add(new SingleDependencyFactory(new TypeMatchesSpecificType(typeof(IProcessRequests)),new BasicDependencyFactory(() => new FrontController(null))));
-    }
-  }
+			//IFetchDependencies container =
+			//  new SimpleContainer(
+			//    new DependencyFactories(all_dependencies,
+			//                            type => new NotImplementedException(
+			//                              string.Format("There is no factory registered that can create {0}", type.Name))),
+			//    (inner, type) =>
+			//      new NotImplementedException(string.Format("There was an error attempting to create a {0}", type.Name), inner));
+
+			//ContainerFacadeResolver resolver = () => container;
+			//Container.facade_resolver = resolver;
+
+			//populate_all_dependency_factories();
+
+
+
+		}
+
+		static void populate_all_dependency_factories()
+		{
+			all_dependencies = new List<ICreateASingleDependency>();
+			all_dependencies.Add(new SingleDependencyFactory(new TypeMatchesSpecificType(typeof(IProcessRequests)), new BasicDependencyFactory(() => new FrontController(null))));
+		}
+	}
 }
